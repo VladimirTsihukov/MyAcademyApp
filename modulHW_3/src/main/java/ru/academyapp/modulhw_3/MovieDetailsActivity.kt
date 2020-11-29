42676c84d030f3265e099994063b89858c98dfed
Vladimir Tsihukov <tishukooov@gmail.com>
2020-11-29 19:40:10 +0300

    create modulHW_3

---
+++ modulHW_3/src/main/java/ru/academyapp/modulhw_3/MovieDetailsActivity.kt
@@ -0,0 +1,11 @@
+package ru.academyapp.modulhw_3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+}
