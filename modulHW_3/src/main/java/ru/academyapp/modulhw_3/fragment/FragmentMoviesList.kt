4b71d290c1c7d2002e43e91dc719937bb8bad819
Vladimir Tsihukov <tishukooov@gmail.com>
2020-11-29 20:29:51 +0300

    add navigation logic. Finish HW_3

--- modulHW_3/src/main/java/ru/academyapp/modulhw_3/fragment/FragmentMoviesList.kt
+++ modulHW_3/src/main/java/ru/academyapp/modulhw_3/fragment/FragmentMoviesList.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import ru.academyapp.modulhw_3.R
 
 class FragmentMoviesList : Fragment() {
@@ -14,4 +16,14 @@ class FragmentMoviesList : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_movies_list, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<ImageView>(R.id.image_film).apply {
+            setOnClickListener {
+                findNavController().navigate(R.id.action_fragmentMoviesList_to_fragmentMoviesDetails)
+            }
+        }
+    }
 }
