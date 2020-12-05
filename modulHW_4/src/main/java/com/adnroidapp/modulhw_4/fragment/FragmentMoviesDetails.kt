48975fc9866a562ace1e129b234cae6723cc1e56
Vladimir Tsihukov <tishukooov@gmail.com>
2020-12-05 23:44:12 +0300

    create modulHW_4

---
+++ modulHW_4/src/main/java/com/adnroidapp/modulhw_4/fragment/FragmentMoviesDetails.kt
@@ -0,0 +1,8 @@
+package com.adnroidapp.modulhw_4.fragment
+
+import androidx.fragment.app.Fragment
+import com.adnroidapp.modulhw_4.R
+
+class FragmentMoviesDetails : Fragment(R.layout.fragment_movies_details) {
+
+}
