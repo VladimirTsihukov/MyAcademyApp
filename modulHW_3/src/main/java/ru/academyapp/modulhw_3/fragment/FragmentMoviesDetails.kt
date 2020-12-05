f3398b3efc51e95dc0ee627ee4b0c82913d71f70
Vladimir Tsihukov <tishukooov@gmail.com>
2020-12-06 00:02:56 +0300

    correct modulHW_3

--- modulHW_3/src/main/java/ru/academyapp/modulhw_3/fragment/FragmentMoviesDetails.kt
+++ modulHW_3/src/main/java/ru/academyapp/modulhw_3/fragment/FragmentMoviesDetails.kt
@@ -1,9 +1,5 @@
 package ru.academyapp.modulhw_3.fragment
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ru.academyapp.modulhw_3.R
 
