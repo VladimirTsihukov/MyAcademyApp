e6618f9886957104b2986b6605bc24306d5b0a87
Vladimir Tsihukov <tishukooov@gmail.com>
2021-01-17 23:29:07 +0300

    Create ViewModel listMovies and ViewModel listDetails through RxJava

---
+++ modulHW_7/src/main/java/com/adnroidapp/modulhw_7/ui/viewModelRxJava/ViewModelMovieDetailsRxJava.kt
@@ -0,0 +1,50 @@
+package com.adnroidapp.modulhw_7.ui.viewModelRxJava
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.adnroidapp.modulhw_7.apiRxJava.ApiFactoryRxJava
+import com.adnroidapp.modulhw_7.apiRxJava.ApiServiceRxJava
+import com.adnroidapp.modulhw_7.data.MovieData
+
+import com.adnroidapp.modulhw_7.pojo.MovieActors
+import com.adnroidapp.modulhw_7.pojo.getMovieDataInfo
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
+class ViewModelMovieDetailsRxJava(application: Application) : AndroidViewModel(application) {
+    private val compositeDisposable = CompositeDisposable()
+    val liveDataMoviesDetails = MutableLiveData<MovieData>()
+    val liveDataMovieActors = MutableLiveData<MovieActors>()
+
+    fun initMovieId(id: Long) {
+        val disposable = ApiFactoryRxJava.apiServiceMoviesRxJava.getMovieInformation(id)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                liveDataMoviesDetails.postValue(it.getMovieDataInfo())
+            }, {
+                Log.d(ApiServiceRxJava.TAG_LOAD, it.message.toString())
+            })
+        compositeDisposable.add(disposable)
+    }
+
+    fun initMovieActors(id: Long) {
+        val disposable = ApiFactoryRxJava.apiServiceMoviesRxJava.getMovieActors(id)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                liveDataMovieActors.postValue(it)
+            },{
+                Log.d(ApiServiceRxJava.TAG_LOAD, it.message.toString())
+            })
+        compositeDisposable.add(disposable)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
+    }
+}
