c2d9d6ad759365525972f2e576eae4c548291ac3
Vladimir Tsihukov <tishukooov@gmail.com>
2021-02-05 02:25:53 +0300

    Created database

---
+++ modulHW_8/src/main/java/com/adnroidapp/modulhw_8/database/DatabaseContact.kt
@@ -0,0 +1,12 @@
+package com.adnroidapp.modulhw_8.database
+
+object DatabaseContact {
+    const val DATABASE_NAME_MOVIES = "Movies.db"
+
+    const val TABLE_NAME_MOVIES_LIST_TOP_RATE = "tableMoviesListTopRate"
+    const val TABLE_NAME_MOVIES_LIST_POPULAR = "tableMoviesListPopular"
+    const val TABLE_NAME_MOVIES_LIST_LIKE = "tableMoviesListLike"
+    const val TABLE_NAME_MOVIES_DETAILS = "tableMoviesDetails"
+    const val SEPARATOR = "_"
+
+}
