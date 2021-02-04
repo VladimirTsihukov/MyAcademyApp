6e45ffc534f89b1b815426ee344176cf0f35406b
Vladimir Tsihukov <tishukooov@gmail.com>
2021-02-05 02:30:50 +0300

    Corrected view model class

--- modulHW_8/src/main/java/com/adnroidapp/modulhw_8/ui/viewModelCoroutine/ViewModelMovieDetailsCoroutine.kt
+++ modulHW_8/src/main/java/com/adnroidapp/modulhw_8/ui/viewModelCoroutine/ViewModelMovieDetailsCoroutine.kt
@@ -3,58 +3,96 @@ package com.adnroidapp.modulhw_8.ui.viewModelCoroutine
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import com.adnroidapp.modulhw_8.R
 import com.adnroidapp.modulhw_8.apiCorutine.ApiFactoryCoroutine
-import com.adnroidapp.modulhw_8.data.MovieData
-import com.adnroidapp.modulhw_8.pojo.MovieActors
+import com.adnroidapp.modulhw_8.database.DatabaseContact.SEPARATOR
+import com.adnroidapp.modulhw_8.database.databaseMoviesList.DbMovies
+import com.adnroidapp.modulhw_8.pojo.ActorsInfo
+import com.adnroidapp.modulhw_8.pojo.getListActor
 import com.adnroidapp.modulhw_8.pojo.getMovieDataInfo
+import com.adnroidapp.modulhw_8.pojo.getMovieDetails
+import com.adnroidapp.modulhw_8.ui.data.MovieData
+import com.adnroidapp.modulhw_8.ui.data.getMovieData
 import kotlinx.coroutines.*
 
 class ViewModelMovieDetailsCoroutine(application: Application) : AndroidViewModel(application) {
 
+    private val error = application.resources.getString(R.string.error_internet_not_connect)
+    private val dbMovieDetails = DbMovies.instance(application)
     val liveDataMoviesDetailsCoroutine = MutableLiveData<MovieData>()
-    val liveDataMovieActorsCoroutine = MutableLiveData<MovieActors>()
+    val liveDataMovieActorsCoroutine = MutableLiveData<List<ActorsInfo>>()
 
     val liveDataErrorServerApi = MutableLiveData<String>()
 
     private val scope = CoroutineScope(Dispatchers.IO)
 
-    fun initMovieIdCoroutine(id: Long) {
+    fun initMovieIdDetails(id: Long) {
         scope.launch {
+            val resultDbDetails = dbMovieDetails.moviesDetails().getMovieDetail(id)
             try {
                 val movieDetails =
                     ApiFactoryCoroutine.apiServiceMovieCor.getMovieByIdAsync(id)
                 if (movieDetails.isSuccessful) {
                     movieDetails.body()?.let {
                         liveDataMoviesDetailsCoroutine.postValue(it.getMovieDataInfo())
+                        dbMovieDetails.moviesDetails().insertMovieDetail(it.getMovieDetails())
+                        initMoviesActors(id)
                     }
                 } else {
+                    getActorsInDb(id)
                     withContext(Dispatchers.Main) {
-                        liveDataErrorServerApi.postValue(movieDetails.code().toString())
+                        resultDbDetails.let {
+                            liveDataMoviesDetailsCoroutine.postValue(getMovieData(it))
+                        }
+                        liveDataErrorServerApi.postValue(error)
                     }
                 }
             } catch (e: Exception) {
-                liveDataErrorServerApi.postValue(e.javaClass.name)
+                getActorsInDb(id)
+                resultDbDetails.let { liveDataMoviesDetailsCoroutine.postValue(getMovieData(it))}
+                liveDataErrorServerApi.postValue(error)
             }
         }
     }
 
-    fun initMoviesActorsCoroutine(id: Long) {
+    private fun initMoviesActors(id: Long) {
         scope.launch {
             try {
                 val movieActors =
                     ApiFactoryCoroutine.apiServiceMovieCor.getMovieActorsCoroutineAsync(id)
                 if (movieActors.isSuccessful) {
-                    movieActors.body()?.let {
-                        liveDataMovieActorsCoroutine.postValue(it)
+                    movieActors.body()?.let { MovieActors ->
+                        liveDataMovieActorsCoroutine.postValue(MovieActors.getListActor(MovieActors.cast))
+                        dbMovieDetails.moviesDetails()
+                            .setNameActors(MovieActors.cast.joinToString(SEPARATOR) { it.name }, id)
+                        dbMovieDetails.moviesDetails()
+                            .setProfilePaths(MovieActors.cast.joinToString(SEPARATOR) {
+                                it.profilePath ?: " "
+                            }, id)
                     }
                 } else {
                     withContext(Dispatchers.Main) {
-                        liveDataErrorServerApi.postValue(movieActors.code().toString())
+                        getActorsInDb(id)
+                        liveDataErrorServerApi.postValue(error)
                     }
                 }
             } catch (e: Exception) {
-                liveDataErrorServerApi.postValue(e.javaClass.name)
+                getActorsInDb(id)
+                liveDataErrorServerApi.postValue(error)
             }
+        }
+    }
+
+    private fun getActorsInDb(id: Long) {
+        val listActorsInfo = mutableListOf<ActorsInfo>()
+        if (!dbMovieDetails.moviesDetails().getNameActors(id).isNullOrEmpty()) {
+            val nameActors = dbMovieDetails.moviesDetails().getNameActors(id).split(SEPARATOR)
+            val listProfilePath =
+                dbMovieDetails.moviesDetails().getProfilePaths(id).split(SEPARATOR)
+            nameActors.forEachIndexed { index, _ ->
+                listActorsInfo.add(ActorsInfo(nameActors[index], listProfilePath[index]))
+            }
+            liveDataMovieActorsCoroutine.postValue(listActorsInfo)
         }
     }
 
--- modulHW_8/src/main/java/com/adnroidapp/modulhw_8/ui/viewModelCoroutine/ViewModelMovieListCoroutine.kt
+++ modulHW_8/src/main/java/com/adnroidapp/modulhw_8/ui/viewModelCoroutine/ViewModelMovieListCoroutine.kt
@@ -3,16 +3,27 @@ package com.adnroidapp.modulhw_8.ui.viewModelCoroutine
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import com.adnroidapp.modulhw_8.R
 import com.adnroidapp.modulhw_8.apiCorutine.ApiFactoryCoroutine
-import com.adnroidapp.modulhw_8.data.MovieData
+import com.adnroidapp.modulhw_8.database.SealedMovies
+import com.adnroidapp.modulhw_8.database.databaseMoviesList.DbMovies
+import com.adnroidapp.modulhw_8.database.dbData.DataDBMoviesPopular
+import com.adnroidapp.modulhw_8.database.dbData.DataDBMoviesTopRate
 import com.adnroidapp.modulhw_8.pojo.MoviesList
+import com.adnroidapp.modulhw_8.pojo.getMovieAllType
 import com.adnroidapp.modulhw_8.pojo.getMovieData
+import com.adnroidapp.modulhw_8.ui.data.MovieData
+import com.adnroidapp.modulhw_8.ui.data.getListMovieData
+import com.adnroidapp.modulhw_8.ui.data.getMovieLike
 import kotlinx.coroutines.*
 import retrofit2.Response
 
 class ViewModelMovieListCoroutine(application: Application) : AndroidViewModel(application) {
 
-    val liveDataMoviesPopularMovies = MutableLiveData<List<MovieData>>()
+    private val error = application.resources.getString(R.string.error_internet_not_connect)
+    private val dbMovies = DbMovies.instance(application)
+
+    val liveDataMoviesList = MutableLiveData<List<MovieData>>()
     val liveDataErrorServerApi = MutableLiveData<String>()
     private val scope = CoroutineScope(Dispatchers.IO)
 
@@ -22,28 +33,116 @@ class ViewModelMovieListCoroutine(application: Application) : AndroidViewModel(a
 
     fun setLoadMoviesPopularMovies() {
         scope.launch {
-            errorHandling(ApiFactoryCoroutine.apiServiceMovieCor.getPopularMoviesAsync())
+            try {
+                errorHandling(ApiFactoryCoroutine.apiServiceMovieCor.getPopularMoviesAsync(),
+                    SealedMovies.MoviesPopular)
+            } catch (e: Exception) {
+                getDBMovies(SealedMovies.MoviesPopular)
+                liveDataErrorServerApi.postValue(error)
+            }
         }
     }
 
     fun setLoadMovieTopRate() {
         scope.launch {
-            errorHandling(ApiFactoryCoroutine.apiServiceMovieCor.getMovieTopRatedAsync())
+            try {
+                errorHandling(ApiFactoryCoroutine.apiServiceMovieCor.getMovieTopRatedAsync(),
+                    SealedMovies.MoviesTopRate)
+            } catch (e: Exception) {
+                getDBMovies(SealedMovies.MoviesTopRate)
+                liveDataErrorServerApi.postValue(error)
+            }
         }
     }
 
-    private suspend fun errorHandling(popularMovies: Response<MoviesList>) {
+    fun getMoviesListLike() {
+        scope.launch {
+            try {
+                val moviesLike = dbMovies.moviesListLike().getAllMoviesLike()
+                liveDataMoviesList.postValue(getListMovieData(SealedMovies.MoviesLike, moviesLike))
+            } catch (e: Exception) {
+                liveDataErrorServerApi.postValue("Error: $e")
+            }
+        }
+    }
+
+    fun addMovieLikeInDb(movie: MovieData) {
+        scope.launch {
+            try {
+                dbMovies.moviesListLike().insertMovieLike(movie.getMovieLike())
+            } catch (e: Exception) {
+                liveDataErrorServerApi.postValue("Error on db")
+            }
+        }
+    }
+
+    fun deleteMoviesLikeInDb(movie: MovieData) {
+        scope.launch {
+            try {
+                dbMovies.moviesListLike().deleteMovieLike(movie.getMovieLike())
+                liveDataMoviesList.postValue(getListMovieData(SealedMovies.MoviesLike,
+                    dbMovies.moviesListLike().getAllMoviesLike()))
+            } catch (e: Exception) {
+                liveDataErrorServerApi.postValue("Error on db")
+            }
+        }
+    }
+
+    private suspend fun errorHandling(
+        popularMovies: Response<MoviesList>,
+        sealedMovies: SealedMovies,
+    ) {
         val movieListResult = mutableListOf<MovieData>()
+        val listIdDetails = dbMovies.moviesListLike().getAllId()
         if (popularMovies.isSuccessful) {
-            popularMovies.body()?.let { result ->
-                result.results.forEach {
+            popularMovies.body()?.let { listMovies ->
+
+                listMovies.results.forEach {
+                    if (listIdDetails.contains(it.id)) {
+                        it.likeMovies = true
+                    }
                     movieListResult.add(it.getMovieData())
                 }
+
+                when (sealedMovies) {
+                    SealedMovies.MoviesPopular -> {
+                        (getMovieAllType(SealedMovies.MoviesPopular,
+                            list = listMovies.results) as List<DataDBMoviesPopular>).let {
+                            dbMovies.moviesPopularDao().insertPopularMoviesList(it)
+                        }
+                    }
+                    SealedMovies.MoviesTopRate -> {
+                        (getMovieAllType(SealedMovies.MoviesTopRate,
+                            list = listMovies.results) as List<DataDBMoviesTopRate>).let {
+                            dbMovies.moviesTopRateDao().insetTopMoviesList(it)
+                        }
+                    }
+                    else -> liveDataErrorServerApi.postValue("Error: not this movie value")
+                }
             }
-            liveDataMoviesPopularMovies.postValue(movieListResult)
+            liveDataMoviesList.postValue(movieListResult)
         } else {
             withContext(Dispatchers.Main) {
                 liveDataErrorServerApi.postValue(popularMovies.code().toString())
+            }
+        }
+    }
+
+    private fun getDBMovies(sealedMovies: SealedMovies) {
+        scope.launch {
+            withContext(Dispatchers.IO) {
+                when (sealedMovies) {
+                    SealedMovies.MoviesPopular ->
+                        liveDataMoviesList.postValue(getListMovieData(sealedMovies,
+                            dbMovies.moviesPopularDao()
+                                .getPopularMoviesList()))
+
+                    SealedMovies.MoviesTopRate ->
+                        liveDataMoviesList.postValue(getListMovieData(sealedMovies,
+                            dbMovies.moviesTopRateDao()
+                                .getTopRateMoviesList()))
+                    SealedMovies.MoviesLike -> TODO()
+                }
             }
         }
     }
